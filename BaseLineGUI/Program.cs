10b8300ec47406d501c10816a70534f774a649c2
Kitra <3056894830@qq.com>
2025-05-22 12:15:11 +0800

    实现xml读取

--- BaseLineGUI/Program.cs
+++ BaseLineGUI/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BaseLineGUI.RulesLoader;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows.Forms;
 
 namespace BaseLineGUI
@@ -11,6 +14,38 @@ namespace BaseLineGUI
         [STAThread]
         static void Main()
         {
+            // 开始读取数据
+            string registryRulesPath = Path.Combine(Application.StartupPath, "Rules", "RegistryRules.xml");
+            string auditPolicyRulesPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Rules", "AuditPolicyRules.xml");
+            List<RegistryRule> registryRules = new List<RegistryRule>();
+            List<AuditPolicyRule> auditPolicyRules = new List<AuditPolicyRule>();
+            // 检查文件是否存在
+            if (!File.Exists(registryRulesPath))
+            {
+                MessageBox.Show($"{registryRulesPath} 文件不存在，请检查路径。");
+                return;
+            }
+            if (!File.Exists(auditPolicyRulesPath))
+            {
+                MessageBox.Show($"{auditPolicyRulesPath} 文件不存在，请检查路径。");
+                return;
+            }
+            // 开始读取
+            try
+            {
+                registryRules = RulesParser.ParseRegistryRules(registryRulesPath);
+                auditPolicyRules = RulesParser.ParseAuditPolicyRule(auditPolicyRulesPath);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"读取规则文件时发生错误，请检查文件格式。\n\n{e}");
+                return;
+            }
+                
+            
+            // MessageBox.Show(registryRules.ToString());
+            // MessageBox.Show(auditPolicyRules.ToString());
+            // 显示程序窗口
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
